0fa026112eebd7baef72a19ed7f8de4a20c5f968
bayazidHossain2 <bh.190140@s.pust.ac.bd>
2022-10-20 01:34:27 +0600

    Location Permission Getter.

--- app/src/main/java/bd/ac/pust/pustvtsunofficial/LoginActivity.java
+++ app/src/main/java/bd/ac/pust/pustvtsunofficial/LoginActivity.java
@@ -1,14 +1,6 @@
 package bd.ac.pust.pustvtsunofficial;
-
 import androidx.appcompat.app.AppCompatActivity;
-import bd.ac.pust.pustvtsunofficial.BusLocationProvider.Adapter.CookieAndSession.CookieManger;
-import bd.ac.pust.pustvtsunofficial.BusLocationProvider.Adapter.TrackerConfig;
-import bd.ac.pust.pustvtsunofficial.BusLocationProvider.Bus.BusFactory;
-
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,15 +8,19 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import java.util.concurrent.TimeUnit;
+import bd.ac.pust.pustvtsunofficial.Helper.LocationPermissionChecker;
+
 
 public class LoginActivity extends AppCompatActivity {
+
     Button loginButton;
     EditText userName;
     EditText password;
     TextView error;
     ImageView logo;
     ProgressBar progressBar;
+    LocationPermissionChecker permissionChecker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,64 +31,72 @@ public class LoginActivity extends AppCompatActivity {
         error=findViewById(R.id.message);
         logo=findViewById(R.id.imageView2);
         progressBar=findViewById(R.id.login_load);
+        permissionChecker = new LocationPermissionChecker(LoginActivity.this);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String usr=userName.getText().toString().trim();
-                String pass=password.getText().toString().trim();
-                if(usr==null||usr.equals("")){
-                    error.setTextColor(Color.RED);
-                    error.setText("You must use a username!");
-                    return;
-                }
-                if(pass==null||pass.equals("")){
-                    error.setTextColor(Color.RED);
-                    error.setText("You must use your password!");
-                    return;
-                }
-                // if both entered, try login.
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    error.setTextColor(Color.BLACK);
-                                    error.setText("Please wait");
-                                    progressBar.setVisibility(View.VISIBLE);
-                                }
-                            });
-                            BusFactory.checkUsernamePassword(usr,pass);
-                            TrackerConfig.updateUserAndPass(usr,pass);
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    error.setTextColor(Color.GREEN);
-                                    error.setText("Login successful");
-                                    Intent i=new Intent(LoginActivity.this,BusFinderActivity.class);
-                                    startActivity(i);
-                                    finish();
-                                }
-                            });
-                        } catch (Exception e) {
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    try {
-                                        CookieManger.getInstance().clearCookies();
-                                    } catch (Exception ex) {
-                                        ex.printStackTrace();
-                                    }
-                                    error.setTextColor(Color.RED);
-                                    error.setText(e.getLocalizedMessage());
-                                    progressBar.setVisibility(View.GONE);
-                                }
-                            });
-                        }
+                    if(permissionChecker.checkPermission()){
+                        error.setText("Permission Granted.");
+                    }else{
+                        error.setText("Permission Not Granted.");
                     }
-                }).start();
+//                String usr=userName.getText().toString().trim();
+//                String pass=password.getText().toString().trim();
+//                if(usr==null||usr.equals("")){
+//                    error.setTextColor(Color.RED);
+//                    error.setText("You must use a username!");
+//                    return;
+//                }
+//                if(pass==null||pass.equals("")){
+//                    error.setTextColor(Color.RED);
+//                    error.setText("You must use your password!");
+//                    return;
+//                }
+//                // if both entered, try login.
+//                new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        try {
+//                            runOnUiThread(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    error.setTextColor(Color.BLACK);
+//                                    error.setText("Please wait");
+//                                    progressBar.setVisibility(View.VISIBLE);
+//                                }
+//                            });
+//                            BusFactory.checkUsernamePassword(usr,pass);
+//                            TrackerConfig.updateUserAndPass(usr,pass);
+//                            runOnUiThread(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    error.setTextColor(Color.GREEN);
+//                                    error.setText("Login successful");
+//                                    Intent i=new Intent(LoginActivity.this,BusFinderActivity.class);
+//                                    startActivity(i);
+//                                    finish();
+//                                }
+//                            });
+//                        } catch (Exception e) {
+//                            runOnUiThread(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    try {
+//                                        CookieManger.getInstance().clearCookies();
+//                                    } catch (Exception ex) {
+//                                        ex.printStackTrace();
+//                                    }
+//                                    error.setTextColor(Color.RED);
+//                                    error.setText(e.getLocalizedMessage());
+//                                    progressBar.setVisibility(View.GONE);
+//                                }
+//                            });
+//                        }
+//                    }
+//                }).start();
             }
         });
     }
+
 }
