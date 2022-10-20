4ffb04906522c51180c52e2496def78820da9635
Sharif Hasan <me.sharif.hasan@gmail.com>
2022-10-21 05:13:25 +0600

    Added enhancements, modifications and focus mode

---
+++ app/src/main/java/bd/ac/pust/pustvtsunofficial/BusLocationProvider/Bus/BusInfo.java
@@ -0,0 +1,10 @@
+package bd.ac.pust.pustvtsunofficial.BusLocationProvider.Bus;
+
+public class BusInfo {
+    public String busName,busRoute,busId;
+    public BusInfo(String id,String name,String route){
+        busId=id;
+        busName=name;
+        busRoute=route;
+    }
+}
--- app/src/main/java/bd/ac/pust/pustvtsunofficial/BusLocationProvider/Config.java
+++ app/src/main/java/bd/ac/pust/pustvtsunofficial/BusLocationProvider/Config.java
@@ -2,10 +2,30 @@ package bd.ac.pust.pustvtsunofficial.BusLocationProvider;
 
 import android.app.Activity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import bd.ac.pust.pustvtsunofficial.BusLocationProvider.Bus.Bus;
+import bd.ac.pust.pustvtsunofficial.BusLocationProvider.Bus.BusInfo;
 import bd.ac.pust.pustvtsunofficial.MainActivity;
 
 public class Config {
-    private Config(){}
+    private Config(){
+        busMapper.put("students",new ArrayList<>());
+        busMapper.put("teachers",new ArrayList<>());
+
+        busMapper.get("students").add(new BusInfo("N/A","SHOW ALL","N?A"));
+        busMapper.get("students").add(new BusInfo("0351510093645193","BUS 1 (BOYS)","Ananta Bazar - Shohor - Meril - Campus"));
+        busMapper.get("students").add(new BusInfo("0351510093643297","BUS 2 (BOYS)","Ananta Bazar - Shohor - Meril - Campus"));
+        busMapper.get("students").add(new BusInfo("0351510093647488","BUS 3 (BOYS)","Ananta Bazar - Shohor - Meril - Campus"));
+        busMapper.get("students").add(new BusInfo("0351510093648122","BUS 4 (BOYS)","Ananta Bazar - Shohor - Meril - Campus"));
+        busMapper.get("students").add(new BusInfo("0351510093645326","BUS 6 (BOYS)","Ananta Bazar - Shohor - Meril - Campus"));
+        busMapper.get("students").add(new BusInfo("0351510093645268","BUS 7 (BOYS)","Ananta Bazar - Shohor - Meril - Campus"));
+        busMapper.get("students").add(new BusInfo("0351510093648080","BUS 8 (BOYS)","Ananta Bazar - Shohor - Meril - Campus"));
+        busMapper.get("students").add(new BusInfo("0351510093646084","BUS 9 (BOYS)","Ananta Bazar - Shohor - Meril - Campus"));
+
+    }
     private Activity mainContext;
     private static Config instance;
 
@@ -20,5 +40,10 @@ public class Config {
     public static Config getInstance() {
         if(instance==null) instance=new Config();
         return instance;
+    }
+    Map<String, ArrayList<BusInfo>> busMapper=new HashMap<>();
+
+    public Map<String, ArrayList<BusInfo>> getBusMapper() {
+        return busMapper;
     }
 }
